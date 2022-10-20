fa0702d665f8ef56223bfaa43a14db58918e435f
momo <291590819@qq.com>
2022-10-20 19:57:33 +0800

    二值化Trackbar

--- main.cpp
+++ main.cpp
@@ -8,21 +8,28 @@
 #include <io.h>
 
 #include"1/1.h"
+//预处理
+int main(void){
 
-int main(){
-
-    cv::Mat img;
-    std::string path="CampusGame 2022-10-19 09-16-27.mp4";
+    cv::Mat img,img_clone;
+    img_clone=img.clone();
+    std::string path="CampusGame.mp4";
+    cv::Mat imgGray,imgHSV,mask,imgDil;
     cv::VideoCapture cap(path);
+
+    int min,max=255;
+    cv::namedWindow("Trackbars",(img.size().width,img.size().height));
+    cv::createTrackbar("Min","Trackbars",&min,255);
+    cv::createTrackbar("Max","Trackbars",&max,255);
     while(1){
         cap.read(img);
-        cv::imshow(" ",img);
+        cv::cvtColor(img,imgGray,cv::COLOR_BGR2GRAY);
+        cv::threshold(imgGray, imgGray, min, max, cv::THRESH_BINARY);
+	    cv::threshold(imgGray, imgGray, 0, 255, cv::THRESH_OTSU);
+        cv::imshow(" ",imgGray);
         cv::waitKey(0);   
     }
-    
 
-
-return 0;
 }
 
 
