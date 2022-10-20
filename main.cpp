e408e7cdcdad0f7ddac5e0aea3f8a44765f8f64d
momo <291590819@qq.com>
2022-10-20 19:15:07 +0800

    视频版本更新

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
-#include<iostream>
+#include <stdio.h>  
+#include <time.h>  
+#include <opencv2/opencv.hpp>  
+#include <iostream> 
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+#include <opencv2/ml/ml.hpp>  
+#include <io.h>
+
 #include"1/1.h"
-using namespace std;
+
 int main(){
 
-std::cout<<"Hello,World!"<<std::endl;
-function();
-system("pause");
+    cv::Mat img;
+    std::string path="CampusGame 2022-10-19 09-16-27.mp4";
+    cv::VideoCapture cap(path);
+    while(1){
+        cap.read(img);
+        cv::imshow(" ",img);
+        cv::waitKey(0);   
+    }
+    
+
+
 return 0;
 }
 
