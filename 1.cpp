26c9ca79cd50fa7047a610dd0183e70d05deaac6
momo <291590819@qq.com>
2022-10-20 14:56:22 +0800

    VScode分文件编写配置

---
+++ 1.cpp
@@ -0,0 +1,4 @@
+#include"1.h"
+void function(){
+    std::cout<<"1111"<<std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
+#include"1.h"
 using namespace std;
 int main(){
 
 std::cout<<"Hello,World!"<<std::endl;
+function();
 system("pause");
 return 0;
 }
