7f1ee89dc495e69ef72bcc3376f338ebf000f511
Chisatooo <291590819@qq.com>
2022-10-22 08:59:28 +0800

    新增Linux运行版本（使用Cmake）

---
+++ CampusGameWindows/GetContours/GetContours.h
@@ -0,0 +1,10 @@
+#include <stdio.h>  
+#include <time.h>  
+#include <opencv2/opencv.hpp>    
+#include <iostream> 
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+#include <opencv2/ml/ml.hpp>  
+#include <io.h> 
+
+void GetContours(cv::Mat img_clone,cv::Mat img);
---
+++ CampusGameWindows/main.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>  
+#include <time.h>  
+#include <opencv2/opencv.hpp>  
+#include <iostream> 
+#include <opencv2/core/core.hpp>  
+#include <opencv2/highgui/highgui.hpp>  
+#include <opencv2/ml/ml.hpp>  
+#include <io.h>
+
+#include"GetContours/GetContours.h"
+//预处理
+int main(void){
+
+    cv::Mat img,img_clone;
+    img_clone=img.clone();
+    std::string path="CampusGame.mp4";
+    cv::VideoCapture cap(path);
+    
+    GetContours(img_clone,img);
+    while(1){
+        cap.read(img_clone);        
+        cv::cvtColor(img_clone,img_clone,cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(img_clone,img_clone,cv::Size(5,5),3,0);
+        //cv::threshold(img_clone, img_clone, 140, 255, cv::THRESH_BINARY);
+	    cv::threshold(img_clone, img_clone, 0, 255, cv::THRESH_OTSU);
+        cv::Mat kernel=cv::getStructuringElement(cv::MORPH_RECT,cv::Size(5,5));
+        cv::erode(img_clone,img_clone,kernel);
+        cv::imshow(" ",img_clone);
+        cv::waitKey(0);   
+    }
+    
+}
+
+
