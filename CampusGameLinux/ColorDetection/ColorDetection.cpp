010ce53b3f55dd93fdb61f44aaedc18e21373e7e
Chisatooo <291590819@qq.com>
2022-10-25 15:56:59 +0800

    为预处理模块编写CmakeLists

---
+++ CampusGameLinux/ColorDetection/ColorDetection.cpp
@@ -0,0 +1,28 @@
+#include"ColorDetcetion.h"
+
+cv::Mat ColorDetcetion(cv::Mat img,bool enemy_color){
+
+    std::vector<cv::Mat> channels;
+    cv::split(img, channels);
+    cv::Mat thresholdImage,mask;
+    
+    //2值化
+    cv::adaptiveThreshold(img, img, 127, cv::ADAPTIVE_THRESH_GAUSSIAN_C, cv::THRESH_BINARY, 3, 10);
+    
+    //目标颜色判断： true红色 false蓝色    通道相减
+    if(enemy_color()){
+        thresholdImage=channels.at(2)-channels.at(1);
+    }
+    else if(!enemy_color()){
+        thresholdImage=channels.at(0)-channels.at(2);
+    }
+    
+    //轮廓增强
+	thresholdImage.copyTo(mask);
+	cv::Canny(mask, mask, 60, 255, 3);
+	cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
+	cv::dilate(mask, mask, element);
+	
+    return mask;
+  
+}
--- CampusGameLinux/GetContours/GetContours.cpp
+++ CampusGameLinux/GetContours/GetContours.cpp
@@ -1,4 +1,26 @@
 #include"GetContours.h"
-void GetContours(cv::Mat img_clone,cv::Mat img){
-    std::cout<<"1111"<<std::endl;
+
+void GetContours(cv::Mat img,cv::Mat img_clone){
+    std::vector<vector<Point>> contours;
+    std::vector<Vec4i> hierarchy;
+    cv::findContours(img_clone,contours,hierarchy,RETR_EXTERNAL,CHAIN_APPROX_SIMPLE);
+    std::vector<Rect> boundRect(contours.size());
+    for(int i=0;i<contours.size();i++){
+        int area=contourArea(contours[i]);
+        cv::boundRect[i]=boundingRect(contours[i]);
+        int h=boundRect[i].height;
+        int w=boundRect[i].width;
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
