d2dae0ab928783e8e1bee5b3e141920de3ab2ff9
Chisatooo <291590819@qq.com>
2022-10-26 23:44:33 +0800

    优化二值化效果

--- CampusGameDemo/CampusGameDemo/ColorDetection/ColorDetection.cpp
+++ CampusGameDemo/CampusGameDemo/ColorDetection/ColorDetection.cpp
@@ -6,7 +6,7 @@ cv::Mat ColorDetection(cv::Mat img){
     vector<Mat> channels;
     split(img, channels);
     Mat thresholdImage,mask;
-    bool enemy_color=0;
+    bool enemy_color=1;
     
     //目标颜色判断： true红色 false蓝色    通道相减
     if(enemy_color){
@@ -15,18 +15,17 @@ cv::Mat ColorDetection(cv::Mat img){
         cv::subtract(channels[0],channels[2],img);
     }
     
-    //灰度化
-    //cv::cvtColor(img,img,cv::COLOR_BGR2GRAY);
-    //imshow(" ",img);
     
     //2值化
     cv::threshold(img, img, 60, 255, cv::THRESH_BINARY);
     
-    // //轮廓增强
-	// thresholdImage.copyTo(mask);
+    //轮廓增强
+	
 	// cv::Canny(mask, mask, 60, 255, 3);
-	// cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
-	// cv::dilate(mask, mask, element);
+	cv::Mat element = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(4, 4));
+	// cv::erode(img, img, element);
+    cv::morphologyEx(img, img, cv::MORPH_OPEN, element);
+    cv::dilate(img, img, element);
 	
     return img;
   
